74a113150703e12fa6eb3c22acc11e4472df565f
david <kaneshiuchiha@gmail.com>
2024-01-08 09:32:18 -0500

    remove loginpage

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-screen flex-col align-middle">
-      <Link href='/login' className='p-4 rounded-xl bg-blue-3'>Login</Link>
+    <main className="flex select-none items-center justify-center h-screen flex-col align-middle">
+      <Link href='/dashboard' className='p-4 rounded bg-white text-black'>Get Started</Link>
     </main>
   )
 }
